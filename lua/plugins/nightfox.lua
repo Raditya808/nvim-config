e70447603c17966c0b52a6a72969b53330dfe791
Raditya808 <mraditradit808@gmail.com>
2025-12-19 17:47:22 +0700

    nightfox theme

---
+++ lua/plugins/nightfox.lua
@@ -0,0 +1,15 @@
+-- nightfox theme
+
+return {
+	"EdenEast/nightfox.nvim",
+	lazy = false,
+	priority = 1000,
+	-- enabled = false,
+	config = function()
+		require("nightfox").setup({
+			options = {
+				dim_inactive = true,
+			},
+		})
+	end,
+} -- lazy
